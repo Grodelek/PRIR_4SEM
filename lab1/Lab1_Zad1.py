7657269cb2229c0c215bdcad833182580aed87e6
A.Grodel <98063447+Grodelek@users.noreply.github.com>
2024-03-05 14:19:10 +0100

    Create Lab1_Zad1.py

---
+++ lab1/Lab1_Zad1.py
@@ -0,0 +1,30 @@
+import threading
+import time
+
+
+def function(i, k):
+    print("start Thread %i\n" % i)
+    time.sleep(k)
+    print("end Thread %i\n" % i)
+    return
+
+#Watki gdzie Powloka czasowa 2s
+t1 = threading.Thread(target=function, args=(1, 2))
+t2 = threading.Thread(target=function, args=(2, 2))
+#Watki gdzie powloka czasowa 3s
+t3 = threading.Thread(target=function, args=(3, 3))
+t4 = threading.Thread(target=function, args=(4, 3))
+t5 = threading.Thread(target=function, args=(5, 3))
+t1.start()
+t2.start()
+
+t3.start()
+t4.start()
+t5.start()
+
+print("End program")
+#Wniosek: Aby wszystkie watki sie wykonaly bez uzycia .join cale wykonanie trwa okolo 3s
+#Jako pierwsze wykonuja sie 2 pierwsze watki ustawione z powloka czasowa 2s a nastepnie po sekundzie nastepne 3.
+#A to dlatego poniewaz wszystkie uruchamiaja sie na raz poniewaz nie uzylismy .join ktore powodowaloby w pewnym rodzaju
+#kolejke, watki czekalyby na zakonczenie sie poprzednich wtedy calosc wykonywala sie 5 sekund
+
